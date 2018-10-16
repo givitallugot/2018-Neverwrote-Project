c5fb586a349336bb8fd471fc10dd3c5d9ba109d4
SUNMI JU <19828233@students.latrobe.edu.au>
2018-10-16 19:58:10 +1100

    backend finished

--- api/src/controllers/notebooks.js
+++ api/src/controllers/notebooks.js
@@ -4,13 +4,52 @@ const models = require('../models');
 
 const router = express.Router();
 
-// Index
+// + Selects only the fields that are allowed to be set by users
+function notebookFilter(obj) {
+  return _.pick(obj, ['title']);
+}
+
+// Index: GET/notebooks
 router.get('/', (req, res) => {
   models.Notebook.findAll({ order: [['createdAt', 'DESC']] })
     .then(notebooks => res.json(notebooks))
     .catch(err => res.status(500).json({ error: err.message }));
 });
 
-/* *** TODO: Fill in the API endpoints for notebooks *** */
+// + Create: POST/notebooks
+router.post('/', (req, res) => {
+  models.Notebook.create(notebookFilter(req.body))
+  .then(notebook => res.json(notebook))
+  .catch(err => res.status(422).json({ error: err.message }));
+});
+
+// + Show: GET/notebooks/:notebookId
+router.get('/:notebookId', (req, res) => {
+  models.Notebook.findById(req.params.notebookId)
+  .then(notebook => res.json(notebook))
+  .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// + Show: GET/notebooks/:notebookId/notes
+router.get('/:notebookId/notes', (req, res) => {
+  models.Note.findAll({ where: { notebookId: req.params.notebookId} })
+  .then(note => res.json(note))
+  .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// + Delete: DELETE/notebooks/:notebookId
+router.delete('/:notebookId', (req, res) => {
+  models.Notebook.destroy({ where: { id: req.params.notebookId } })
+  .then(() => res.json({}))
+  .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// + Update: PUT/notebooks/:notebookId
+router.put('/:notebookId', (req, res) => {
+  models.Notebook.findById(req.params.notebookId)
+    .then(notebook => notebook.update(notebookFilter(req.body)))
+    .then(notebook => res.json(notebook))
+    .catch(err => res.status(422).json({ error: err.message }));
+});
 
 module.exports = router;
--- api/src/controllers/notes.js
+++ api/src/controllers/notes.js
@@ -4,6 +4,45 @@ const models = require('../models');
 
 const router = express.Router();
 
-/* *** TODO: Fill in the API endpoints for notes *** */
+// + Selects only the fields that are allowed to be set by users + notebookID?
+function noteFilter(obj) {
+  return _.pick(obj, ['notebookId', 'title', 'content']);
+}
+
+// + Index: GET/notes
+router.get('/', (req, res) => {
+  models.Note.findAll({ order: [['createdAt', 'DESC']] })
+  .then(notes => res.json(notes))
+  .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// + Create: POST/notes
+router.post('/', (req, res) => {
+  models.Note.create(noteFilter(req.body))
+  .then(note => res.json(note))
+  .catch(err => res.status(422).json({ error: err.message }));
+});
+
+// + Show: GET/notes/:noteId
+router.get('/:noteId', (req, res) => {
+  models.Note.findById(req.params.noteId)
+  .then(note => res.json(note))
+  .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// + Delete: DELETE/notes/:noteId
+router.delete('/:noteId', (req, res) => {
+  models.Note.destroy({ where: { id: req.params.noteId } })
+  .then(() => res.json({}))
+  .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// + Update: PUT/notes/:noteId
+router.put('/:noteId', (req, res) => {
+  models.Note.findById(req.params.noteId)
+    .then(note => note.update(noteFilter(req.body)))
+    .then(note => res.json(note))
+    .catch(err => res.status(422).json({ error: err.message }));
+});
 
 module.exports = router;
